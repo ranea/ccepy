1346866b2c55c243220600b434a3fa9e711e4e64
Adrian Ranea <adrianranea@gmail.com>
2016-06-23 16:53:26 +0200

    añadido esquema ECDSA y sus test

--- ccepy/esquemas_criptograficos.py
+++ ccepy/esquemas_criptograficos.py
@@ -1,8 +1,10 @@
 import random
+import hashlib
 # from math import gcd
 
 # from ccepy.cuerpos_finitos import Fq
 from ccepy.curvas_elipticas import curva_eliptica_sobre_Fq
+from ccepy.aritmetica_elemental import Zp, alg_euclides
 
 
 # añadir ref: sec 1 3.3
@@ -14,56 +16,81 @@ class ECDH(object):
         self.orden = orden
 
         # generamos las llaves
-        self.llave_publica = random.randrange(1, self.orden)
-        self.llave_privada = self.llave_publica * self.generador
+        self.llave_privada = random.randrange(1, self.orden)
+        self.llave_publica = self.llave_privada * self.generador
 
     def calcula_secreto_compartido(self, otra_llave_publica):
-        self.secreto_compartido = self.llave_privada * otra_llave_publica
+        self.secreto_compartido = (self.llave_privada * otra_llave_publica).x
 
 
-# ParametrosCurvaElipticaSobreFq = namedtuple('CurvaElípticaFq', ['nombre', 'p', 'a', 'b', 'x1', 'y1'])
-#
-# ejemplo_curva_eliptica = ParametrosCurvaElipticaSobreFq(
-#     nombre="ejemplo1",
-#     p=1287,
-#     a=3851,
-#     b=324,
-#     x1=920,
-#     y1=303,
-#     orden=8
-# )
-#
-# def fname(ce):
-#     E = curva_eliptica_sobre_Fq(ce.a, ce.b, ce.p)
-#     generador = E(ce.x1, ce.y1)
-#     orden = ce.orden
-#     return E, generador, orden
+class ECDSA(object):
+    """docstring for ECDH"""
+    def __init__(self, curva_eliptica, generador, orden):
+        if hasattr(curva_eliptica.Fq, 'n'):
+            raise ValueError("El cardinal del cuerpo finito debe ser un número primo.")
 
-if __name__ == '__main__':
-    pass
+        self.curva_eliptica = curva_eliptica
+        self.generador = generador
+        self.orden = orden  # debe ser primo
 
+        # generamos las llaves
+        self.llave_privada = random.randrange(1, self.orden)
+        self.llave_publica = self.llave_privada * self.generador
 
+    # TODO: ref 4.29 guide
+    def firma(self, mensaje):
+        """..."""
+        # renombramos las variables para usar la notación de la referencia
+        P = self.generador
+        n = self.orden
+        d = self.llave_privada
+        m = mensaje
 
-    # # Example 2: Secp256k1
-    # p = int("0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16)
-    # a = 0
-    # b = 7
-    # order = int("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
-    # x_g = int("0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16)
-    # y_g = int("0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16)
-    # ec = EllipticCurveOverZp(p, a, b)
-    # alice = ECDH(ec, ec(x_g, y_g), order, 8)
-    # print(alice)
-    #
-    # bob = ECDH(ec, ec(x_g, y_g), order, 8)
-    # print(bob)
-    #
-    # alice_public_key = alice.send_public_key()
-    # bob_public_key = bob.send_public_key()
-    #
-    # alice.get_shared_secret(bob_public_key)
-    # bob.get_shared_secret(alice_public_key)
-    # print()
-    # print(alice.shared_secret)
-    # print(bob.shared_secret)
-    # print(alice.shared_secret == bob.shared_secret)
+        while True:
+            k = random.randrange(1, self.orden - 1)
+            kP = k * P
+
+            Zn = Zp(n)
+            r = Zn(kP.x)
+            if r == 0:
+                continue
+
+            hash_mensaje = hashlib.sha1(bytes(m, 'utf-8')).digest()
+            e = int.from_bytes(hash_mensaje[:n.bit_length()], byteorder='big')
+
+            inverso_k = Zn(k).inverso()
+            s = inverso_k * (e + d * r)
+            if s == 0:
+                continue
+
+            return int(r), int(s)
+
+    # TODO: ref 4.30 guide
+    def verifica(self, mensaje, r, s, llave_publica_firmador):
+        # renombramos las variables para usar la notación de la referencia
+        P = self.generador
+        n = self.orden
+        Q = llave_publica_firmador
+        m = mensaje
+
+        if not (1 <= r <= n - 1 and 1 <= s <= n - 1):
+            return False
+
+        hash_mensaje = hashlib.sha1(bytes(m, 'utf-8')).digest()
+        e = int.from_bytes(hash_mensaje[:n.bit_length()], byteorder='big')
+
+        Zn = Zp(n)
+        w = Zn(s).inverso()
+        u1 = int(e * w)
+        u2 = int(r * w)
+
+        X = u1 * P + u2 * Q
+
+        if X.es_elemento_neutro():
+            return False
+
+        v = Zn(X.x)
+        if v == r:
+            return True
+        else:
+            return False
--- tests/test_esquemas_criptograficos.py
+++ tests/test_esquemas_criptograficos.py
@@ -4,10 +4,10 @@ import unittest
 import random
 
 from hypothesis import given, assume
-from hypothesis.strategies import integers, sampled_from
+from hypothesis.strategies import integers, sampled_from, text
 
 # from ccepy import esquemas_criptograficos  # para cargar los docstring
-from ccepy.esquemas_criptograficos import ECDH
+from ccepy.esquemas_criptograficos import ECDH, ECDSA
 from ccepy.curvas_elipticas import curva_eliptica_sobre_Fq
 from ccepy.listado_curvas_elipticas import curvas_eliptipcas_sobre_Fq_famosas
 from ccepy.listado_curvas_elipticas import procesar_parametros_curva_eliptica
@@ -20,9 +20,9 @@ class TestECDH(unittest.TestCase):
         random.seed(5040)
 
     @given(sampled_from(curvas_eliptipcas_sobre_Fq_famosas))
-    def test(self, ce):
+    def test_ECDH(self, ce):
         E, generador, orden = procesar_parametros_curva_eliptica(ce)
-        print(ce.nombre)
+        # print(ce.nombre)
 
         alicia = ECDH(E, generador, orden)
         bob = ECDH(E, generador, orden)
@@ -36,6 +36,41 @@ class TestECDH(unittest.TestCase):
         #                                                                             bob.secreto_compartido))
         assert alicia.secreto_compartido == bob.secreto_compartido
 
+        eva = ECDH(E, generador, orden)  # eva impersona a alicia
+        assume(eva.llave_privada != alicia.llave_privada)
+        eva.calcula_secreto_compartido(bob.llave_publica)
+        assert eva.secreto_compartido != alicia.secreto_compartido
+
+
+class TestECDSA(unittest.TestCase):
+    """Conjuto de test para ECDSA"""
+    @classmethod
+    def setUpClass(cls):
+        random.seed(5040)
+
+    @given(sampled_from(curvas_eliptipcas_sobre_Fq_famosas), text(), text())
+    def test_ECDSA(self, ce, mensaje, otro_mensaje):
+        E, generador, orden = procesar_parametros_curva_eliptica(ce)
+        # print(ce.nombre, "|", mensaje, "|")
+
+        alicia = ECDSA(E, generador, orden)
+        bob = ECDSA(E, generador, orden)
+        # print("\n\nMensaje:", mensaje)
+        # print("Llaves de Alicia:\n\tPública: {0}\n\tPrivada: {1}".format(alicia.llave_publica,
+        #                                                             alicia.llave_privada))
+        r, s = alicia.firma(mensaje)
+        # print("r:", r, "\ns:", s)
+        assert bob.verifica(mensaje, r, s, alicia.llave_publica)
+
+        assume(mensaje != otro_mensaje)
+        # print("Otro mensaje:", otro_mensaje)
+        assert not bob.verifica(otro_mensaje, r, s, alicia.llave_publica)
+
+        eva = ECDSA(E, generador, orden)
+        assume(eva.llave_privada != alicia.llave_privada)
+        assert not bob.verifica(mensaje, r, s, eva.llave_publica)
+        rr, ss = eva.firma(mensaje)
+        assert not bob.verifica(mensaje, rr, ss, alicia.llave_publica)
 
 
 if __name__ == '__main__':
@@ -57,4 +92,25 @@ if __name__ == '__main__':
     # print("Secreto compartido calculado por:\n\tAlicia: {0}\n\tBob: {1}".format(alicia.secreto_compartido,
     #                                                                             bob.secreto_compartido))
     # print("¿Es el mismo secreto? {0}".format(alicia.secreto_compartido == bob.secreto_compartido))
+
+    # E = curva_eliptica_sobre_Fq(a=5, b=7, p=113)
+    # generador = E(16, 51)
+    #
+    # def orden_punto_curva_eliptica(punto):
+    #     i = 1
+    #     P = 1 * punto  # una copia
+    #     for j in range(1, 128):
+    #         if P.es_elemento_neutro():
+    #             return i
+    #         P += punto
+    #         i += 1
+    #
+    # orden = orden_punto_curva_eliptica(generador)
+    # mensaje = "It is possible to write endlessly on elliptic curves."
+    # alicia = ECDSA(E, generador, orden)
+    # bob = ECDSA(E, generador, orden)
+    #
+    # r, s = alicia.firma(mensaje)
+    # print(bob.verifica(mensaje, r, s, alicia.llave_publica))
+
     unittest.main()
